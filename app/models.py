c4edc079d72cffb8da977d82131c797b7d92abf6
MrazTevin <millatevin@gmail.com>
2018-02-09 23:29:45 +0300

    migrations

--- app/models.py
+++ app/models.py
@@ -1,12 +1,16 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask.ext.login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(255))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
 
     @property
     def password(self):
