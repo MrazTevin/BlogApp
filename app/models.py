41f74c42dee7b9ef5fbbfd84e6fd51e58e32f683
MrazTevin <millatevin@gmail.com>
2018-02-10 12:18:41 +0300

    user registration

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(255))
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
 
