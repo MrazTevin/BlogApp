d1e83f7e308b783745eef7dbd453a431feb9f723
MrazTevin <millatevin@gmail.com>
2018-02-10 07:40:17 +0300

    log users in and out

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from flask_login import login_user
+from flask import render_template,redirect,request,request,url_for,flash
+from flask_login import login_user, logout_user, login_required
 from .forms import LoginForm
+from ..models import User
 from . import auth
 
 
@@ -9,8 +10,18 @@ from . import auth
 
 @auth.route('/login')
 def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user_verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next')) or url_for('main.index')
+        flash('Invalid username or password')
+    return render_template('auth/login.html', form=form)
 
-    '''
-    will return the index page and its data
-    '''
-    return render_template('auth/login.html')
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index')) 
